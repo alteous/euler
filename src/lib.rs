864f2882d32239341575add29bb1d4b9b73882d8
David Harvey-Macaulay <alteous@outlook.com>
2017-09-08 09:38:49 +0100

    Add vector and matrix types

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,212 @@
+#![allow(unused_macros)]
+
 extern crate cgmath;
+
+use std::mem;
+
+#[macro_use]
+mod macros;
+
+/// 2D vector.
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[repr(C)]
+pub struct Vec2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+/// 3D vector.
+///
+/// # Note
+///
+/// The representation is a homogeneous 3D vector with a fixed `w` value of 1.0.
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[repr(C)]
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    #[doc(hidden)]
+    pub w: f32,
+}
+
+/// Homogeneous 3D vector.
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[repr(C)]
+pub struct Vec4 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
+}
+
+/// 2x2 column major matrix.
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[repr(C)]
+pub struct Mat2 {
+    pub m00: f32,
+    pub m01: f32,
+
+    pub m10: f32,
+    pub m11: f32,
+}
+
+/// 3x3 column major matrix.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Mat3 {
+    pub m00: f32,
+    pub m01: f32,
+    pub m02: f32,
+
+    pub m10: f32,
+    pub m11: f32,
+    pub m12: f32,
+
+    pub m20: f32,
+    pub m21: f32,
+    pub m22: f32,
+}
+
+/// 4x4 column major matrix.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Mat4 {
+    pub m00: f32,
+    pub m01: f32,
+    pub m02: f32,
+    pub m03: f32,
+
+    pub m10: f32,
+    pub m11: f32,
+    pub m12: f32,
+    pub m13: f32,
+
+    pub m20: f32,
+    pub m21: f32,
+    pub m22: f32,
+    pub m23: f32,
+
+    pub m30: f32,
+    pub m31: f32,
+    pub m32: f32,
+    pub m33: f32,
+}
+
+impl From<Vec3> for Vec2 {
+    fn from(vec3: Vec3) -> Vec2 {
+        Vec2 { x: vec3.x, y: vec3.y }
+    }
+}
+
+impl From<Vec4> for Vec2 {
+    fn from(vec4: Vec4) -> Vec2 {
+        Vec2 { x: vec4.x, y: vec4.y }
+    }
+}
+
+impl From<Vec4> for Vec3 {
+    fn from(vec4: Vec4) -> Vec3 {
+        Vec3 { x: vec4.x, y: vec4.y, z: vec4.z, w: 1.0 }
+    }
+}
+
+impl AsRef<[f32; 2]> for Vec2 {
+    fn as_ref(&self) -> &[f32; 2] {
+        unsafe {
+            mem::transmute(self)
+        }
+    }
+}
+
+impl AsRef<[f32; 3]> for Vec3 {
+    fn as_ref(&self) -> &[f32; 3] {
+        unsafe {
+            mem::transmute(self)
+        }
+    }
+}
+
+impl AsRef<[f32; 4]> for Vec4 {
+    fn as_ref(&self) -> &[f32; 4] {
+        unsafe {
+            mem::transmute(self)
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn vec2_macro_empty() {
+        let zero = vec2!();
+        assert_eq!(zero.as_ref(), &[0.0, 0.0]);
+    }    
+
+    #[test]
+    fn vec2_macro_ones() {
+        let ones = vec2!(1.0);
+        assert_eq!(ones.as_ref(), &[1.0, 1.0]);
+    }
+
+    #[test]
+    fn vec2_macro_full() {
+        let full = vec2!(1.2, 3.4);
+        assert_eq!(full.as_ref(), &[1.2, 3.4]);
+    }
+
+    #[test]
+    fn vec3_macro_zeros() {
+        let zeros = vec3!();
+        assert_eq!(zeros.as_ref(), &[0.0, 0.0, 0.0]);
+    }
+
+    #[test]
+    fn vec3_macro_ones() {
+        let ones = vec3!(1.0);
+        assert_eq!(ones.as_ref(), &[1.0, 1.0, 1.0]);
+    }
+
+    #[test]
+    fn vec3_macro_full() {
+        let full = vec3!(1.2, 3.4, 5.6);
+        assert_eq!(full.as_ref(), &[1.2, 3.4, 5.6]);
+    }
+
+    #[test]
+    fn vec3_macro_from_vec2() {
+        let vec2 = vec2!(1.2, 3.4);
+        let vec3 = vec3!(vec2, 5.6);
+        assert_eq!(vec3.as_ref(), &[1.2, 3.4, 5.6]);
+    }
+
+    #[test]
+    fn vec4_macro_zeros() {
+        let zeros = vec4!();
+        assert_eq!(zeros.as_ref(), &[0.0, 0.0, 0.0, 0.0]);
+    }
+
+    #[test]
+    fn vec4_macro_ones() {
+        let ones = vec4!(1.0);
+        assert_eq!(ones.as_ref(), &[1.0, 1.0, 1.0, 1.0]);
+    }
+
+    #[test]
+    fn vec4_macro_full() {
+        let full = vec4!(1.2, 3.4, 5.6, 7.8);
+        assert_eq!(full.as_ref(), &[1.2, 3.4, 5.6, 7.8]);
+    }
+
+    #[test]
+    fn vec4_macro_from_vec2() {
+        let vec2 = vec2!(1.2, 3.4);
+        let vec4 = vec4!(vec2, 5.6, 7.8);
+        assert_eq!(vec4.as_ref(), &[1.2, 3.4, 5.6, 7.8]);
+    }
+
+    #[test]
+    fn vec4_macro_from_vec3() {
+        let vec3 = vec3!(1.2, 3.4, 5.6);
+        let vec4 = vec4!(vec3, 7.8);
+        assert_eq!(vec4.as_ref(), &[1.2, 3.4, 5.6, 7.8]);
     }
 }
---
+++ src/macros.rs
@@ -0,0 +1,168 @@
+
+/// 2D vector macro constructor.
+///
+/// # Examples
+///
+/// Zeros
+///
+/// ```rust,ignore
+/// let zeros = vec2!();
+/// assert_eq!(zeros.as_ref(), &[0.0, 0.0]);
+/// ```
+///
+/// Ones
+///
+/// ```rust,ignore
+/// let ones = vec2!(1.0);
+/// assert_eq!(ones.as_ref(), &[1.0, 1.0]);
+/// ```
+///
+/// Full
+///
+/// ```rust,ignore
+/// let full = vec2!(1.2, 3.4);
+/// assert_eq!(full.as_ref(), &[1.2, 3.4]);
+/// ```
+#[macro_export]
+macro_rules! vec2 {
+    () => {
+        $crate::Vec2 { x: 0.0, y: 0.0 }
+    };
+
+    ($x:expr) => {
+        $crate::Vec2 { x: $x, y: $x }
+    };
+
+    ($x:expr, $y:expr) => {
+        $crate::Vec2 { x: $x, y: $y }
+    };
+}
+
+/// 3D vector macro constructor.
+///
+/// # Examples
+///
+/// Zeros
+///
+/// ```rust,ignore
+/// let zeros = vec3!();
+/// assert_eq!(zeros.as_ref(), &[0.0, 0.0]);
+/// ```
+///
+/// Ones
+///
+/// ```rust,ignore
+/// let ones = vec3!(1.0);
+/// assert_eq!(ones.as_ref(), &[1.0, 1.0, 1.0]);
+/// ```
+///
+/// Full
+///
+/// ```rust,ignore
+/// let full = vec3!(1.2, 3.4, 5.6);
+/// assert_eq!(full.as_ref(), &[1.2, 3.4, 5.6]);
+/// ```
+///
+/// From `vec2`
+///
+/// ```rust,ignore
+/// let vec2 = vec2!(1.2, 3.4);
+/// let vec3 = vec3!(vec2, 5.6);
+/// assert_eq!(vec3.as_ref(), &[1.2, 3.4, 5.6]);
+/// ```
+#[macro_export]
+macro_rules! vec3 {
+    () => {
+        $crate::Vec3 { x: 0.0, y: 0.0, z: 0.0, w: 1.0 }
+    };
+
+    ($xyz:expr) => {
+        $crate::Vec3 { x: $xyz, y: $xyz, z: $xyz, w: 1.0 }
+    };
+    
+    ($xy:expr, $z:expr) => {
+        $crate::Vec3 {
+            x: $crate::Vec2::from($xy).x,
+            y: $crate::Vec2::from($xy).y,
+            z: $z,
+            w: 1.0,
+        }
+    };
+
+    ($x:expr, $y:expr, $z:expr) => {
+        $crate::Vec3 { x: $x, y: $y, z: $z, w: 1.0 }
+    };
+}
+
+/// Homogeneous 3D vector macro constructor.
+///
+/// # Examples
+///
+/// Zeros
+///
+/// ```rust,ignore
+/// let zeros = vec4!();
+/// assert_eq!(zeros.as_ref(), &[0.0, 0.0, 0.0, 0.0]);
+/// ```
+///
+/// Ones
+///
+/// ```rust,ignore
+/// let ones = vec4!(1.0);
+/// assert_eq!(ones.as_ref(), &[1.0, 1.0, 1.0, 1.0]);
+/// ```
+///
+/// Full
+///
+/// ```rust,ignore
+/// let full = vec4!(1.2, 3.4, 5.6, 7.8);
+/// assert_eq!(full.as_ref(), &[1.2, 3.4, 5.6, 7.8]);
+/// ```
+///
+/// From `vec2`
+///
+/// ```rust,ignore
+/// let vec2 = vec2!(1.2, 3.4);
+/// let vec4 = vec4!(vec2, 5.6, 7.8);
+/// assert_eq!(vec3.as_ref(), &[1.2, 3.4, 5.6, 7.8]);
+/// ```
+///
+/// From `vec3`
+///
+/// ```rust,ignore
+/// let vec3 = vec3!(1.2, 3.4, 5.6);
+/// let vec4 = vec4!(vec3, 7.8);
+/// assert_eq!(vec4.as_ref(), &[1.2, 3.4, 5.6, 7.8]);
+/// ```
+#[macro_export]
+macro_rules! vec4 {
+    () => {
+        $crate::Vec4 { x: 0.0, y: 0.0, z: 0.0, w: 0.0 }
+    };
+
+    ($xyzw:expr) => {
+        $crate::Vec4 { x: $xyzw, y: $xyzw, z: $xyzw, w: $xyzw }
+    };
+
+    ($xyz:expr, $w:expr) => {
+        $crate::Vec4 {
+            x: $crate::Vec3::from($xyz).x,
+            y: $crate::Vec3::from($xyz).y,
+            z: $crate::Vec3::from($xyz).z,
+            w: $w,
+        }
+    };
+
+    ($xy:expr, $z:expr, $w:expr) => {
+        $crate::Vec4 {
+            x: $crate::Vec2::from($xy).x,
+            y: $crate::Vec2::from($xy).y,
+            z: $z,
+            w: $w,
+        }
+    };
+
+    ($x:expr, $y:expr, $z:expr, $w:expr) => {
+        $crate::Vec4 { x: $x, y: $y, z: $z, w: $w }
+    };
+}
